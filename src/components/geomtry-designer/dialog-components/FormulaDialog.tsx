1ae2ed1b63da928f618a0aedb36a6709a9adae56
kartGRAM <ahuaya1024@gmail.com>
2022-07-18 01:05:16 +0900

    FormulaDialog更新

--- src/components/geomtry-designer/dialog-components/FormulaDialog.tsx
+++ src/components/geomtry-designer/dialog-components/FormulaDialog.tsx
@@ -1,15 +1,34 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import ListItemText from '@mui/material/ListItemText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Dialog, {DialogProps} from '@mui/material/Dialog';
-import AddIcon from '@mui/icons-material/Add';
 import {useSelector, useDispatch} from 'react-redux';
 import {RootState} from '@store/store';
 import {setFormulaDialogOpen} from '@store/reducers/uiTempGeometryDesigner';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number
+) {
+  return {name, calories, fat, carbs, protein};
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9)
+];
 
 export function FormulaDialog(props: DialogProps) {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -27,16 +46,35 @@ export function FormulaDialog(props: DialogProps) {
   return (
     <Dialog {...props} onClose={handleClose} open={formulaDialogOpen}>
       <DialogTitle>Set backup account</DialogTitle>
-      <List sx={{pt: 0}}>
-        <ListItem autoFocus>
-          <ListItemAvatar>
-            <Avatar>
-              <AddIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Add account" />
-        </ListItem>
-      </List>
+      <TableContainer component={Paper}>
+        <Table sx={{minWidth: 650}} size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Dessert (100g serving)</TableCell>
+              <TableCell align="right">Calories</TableCell>
+              <TableCell align="right">Fat&nbsp;(g)</TableCell>
+              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{'&:last-child td, &:last-child th': {border: 0}}}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Dialog>
   );
 }
