6e5b598d43230d705493bf829e3005ed308e2fed
kartGRAM <ahuaya1024@gmail.com>
2022-09-10 00:31:16 +0900

    表示が消えるように変更

--- src/components/geomtry-designer/r3f-components/NodeSphere.tsx
+++ src/components/geomtry-designer/r3f-components/NodeSphere.tsx
@@ -5,7 +5,7 @@ import {Sphere} from '@react-three/drei';
 import {useSelector, useDispatch} from 'react-redux';
 import {selectElement} from '@app/store/reducers/uiTempGeometryDesigner';
 import store, {RootState} from '@store/store';
-import {trans} from '@gd/IElements';
+import {trans, isElement} from '@gd/IElements';
 import {getMatrix3} from '@gd/NamedValues';
 import {INamedVector3} from '@gd/INamedValues';
 import {setSelectedPoint} from '@store/reducers/uiTempGeometryDesigner';
@@ -15,7 +15,8 @@ const NodeSphere = (props: {node: INamedVector3}) => {
   const coMatrix = getMatrix3(
     useSelector((state: RootState) => state.dgd.present.transCoordinateMatrix)
   );
-  const ref = React.useRef<THREE.MeshBasicMaterial>(null!);
+  const materialRef = React.useRef<THREE.MeshBasicMaterial>(null);
+  const meshRef = React.useRef<THREE.Mesh>(null);
 
   const dispatch = useDispatch();
 
@@ -35,20 +36,24 @@ const NodeSphere = (props: {node: INamedVector3}) => {
     if (isSelected) {
       color = isSelected.color ?? 0xff0000;
     }
-    if (ref.current) {
-      ref.current.color.set(color);
+    if (materialRef.current) {
+      materialRef.current.color.set(color);
+    }
+    if (isElement(node.parent) && meshRef.current) {
+      meshRef.current.visible = node.parent.visible.value ?? false;
     }
   });
 
   return (
     <Sphere
+      ref={meshRef}
       position={trans(node, coMatrix)}
       args={[5, 16, 16]}
       onDoubleClick={(e) => {
         handleOnDoubleClick(e);
       }}
     >
-      <meshBasicMaterial color={0x00ff00} ref={ref} />
+      <meshBasicMaterial color={0x00ff00} ref={materialRef} />
     </Sphere>
   );
 };
