3dcb0cf27c910da20793a19f070b17f2b25bc8d2
kartGRAM <ahuaya1024@gmail.com>
2022-09-17 09:43:09 +0900

    AABBを表示

--- src/components/geomtry-designer/r3f-components/CollectedAssembly.tsx
+++ src/components/geomtry-designer/r3f-components/CollectedAssembly.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import * as THREE from 'three';
+import {useFrame} from '@react-three/fiber';
+import {Box} from '@react-three/drei';
 import {useSelector} from 'react-redux';
 import {RootState} from '@store/store';
 import {isBody} from '@gd/IElements';
@@ -8,14 +11,35 @@ const CollectedAssembly = () => {
   const assembly = useSelector(
     (state: RootState) => state.uitgd.collectedAssembly
   );
-  if (!assembly) return null;
-  const {children} = assembly;
+  const groupRef = React.useRef<THREE.Group>(null!);
+  const boxRef = React.useRef<THREE.Mesh>(null!);
+  const children = assembly?.children ?? [];
+  useFrame(() => {
+    const box3 = new THREE.Box3().setFromObject(groupRef.current);
+    const dimensions = new THREE.Vector3().subVectors(box3.max, box3.min);
+    const boxGeo = new THREE.BoxBufferGeometry(
+      dimensions.x,
+      dimensions.y,
+      dimensions.z
+    );
+    // move new mesh center so it's aligned with the original object
+    const matrix = new THREE.Matrix4().setPosition(
+      dimensions.addVectors(box3.min, box3.max).multiplyScalar(0.5)
+    );
+    boxGeo.applyMatrix4(matrix);
+    boxRef.current.geometry.copy(boxGeo);
+  });
   return (
     <>
-      {children.map((child) => {
-        if (isBody(child)) return <Body element={child} key={child.nodeID} />;
-        return null;
-      })}
+      <group ref={groupRef}>
+        {children.map((child) => {
+          if (isBody(child)) return <Body element={child} key={child.nodeID} />;
+          return null;
+        })}
+      </group>
+      <Box ref={boxRef}>
+        <meshBasicMaterial color="hotpink" wireframe wireframeLinewidth={3} />
+      </Box>
     </>
   );
 };
