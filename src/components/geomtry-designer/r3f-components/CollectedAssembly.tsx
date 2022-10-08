6b676c48b81e1df144958e777e3ec97b915c2702
kartGRAM <ahuaya1024@gmail.com>
2022-10-08 16:38:19 +0900

    math.jsのマトリックスを使用している最後のコミット

--- src/components/geomtry-designer/r3f-components/CollectedAssembly.tsx
+++ src/components/geomtry-designer/r3f-components/CollectedAssembly.tsx
@@ -2,9 +2,10 @@ import * as React from 'react';
 import * as THREE from 'three';
 import {useSelector} from 'react-redux';
 import {RootState} from '@store/store';
-import {isBody, isAArm} from '@gd/IElements';
+import {isBody, isAArm, isBar} from '@gd/IElements';
 import Body from './Body';
 import AArm from './AArm';
+import Bar from './Bar';
 
 const CollectedAssembly = () => {
   const assembly = useSelector(
@@ -19,6 +20,7 @@ const CollectedAssembly = () => {
       {children.map((child) => {
         if (isBody(child)) return <Body element={child} key={child.nodeID} />;
         if (isAArm(child)) return <AArm element={child} key={child.nodeID} />;
+        if (isBar(child)) return <Bar element={child} key={child.nodeID} />;
         return null;
       })}
     </group>
