a52af28ed6b9dde9c22a012a4c5a9d98e58f6f5a
kartGRAM <ahuaya1024@gmail.com>
2022-11-26 00:03:38 +0900

    編集中

--- src/components/geomtry-designer/side-panel-components/controllers/LinearBushingControl.tsx
+++ src/components/geomtry-designer/side-panel-components/controllers/LinearBushingControl.tsx
@@ -7,6 +7,8 @@ import TextField, {OutlinedTextFieldProps} from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
 import Slider from '@mui/material/Slider';
 import Box from '@mui/material/Box';
+import {InputBaseComponentProps} from '@mui/material/InputBase';
+import {isNumber} from '@app/utils/helpers';
 
 export interface LinearBushingControlProps {
   control: IControl | undefined;
@@ -15,20 +17,53 @@ export interface LinearBushingControlProps {
 
 export function LinearBushingControl(props: LinearBushingControlProps) {
   const {control, element} = props;
+  const [speed, setSpeed] = React.useState<number | ''>(10);
+  const max = 100;
+  const min = 0;
+
+  const handleSliderSpeedChange = (
+    event: Event,
+    newValue: number | number[]
+  ) => {
+    if (!isNumber(newValue)) newValue = newValue.shift() ?? 0;
+    setSpeed(newValue);
+  };
+  const handleInputSpeedChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setSpeed(event.target.value === '' ? '' : Number(event.target.value));
+  };
+
+  const handleBlur = () => {
+    if (isNumber(speed) && speed < min) setSpeed(min);
+  };
+
   return (
     <Box
       component="div"
       sx={{display: 'flex', flexDirection: 'row', width: '100%'}}
     >
-      <ValueField label="Speed" name="speed" variant="outlined" unit="mm/s" />
       <Box component="div" sx={{flexGrow: 1, mt: 0.7}}>
         <Slider
           size="small"
-          defaultValue={70}
+          defaultValue={10}
           aria-label="Small"
           valueLabelDisplay="auto"
+          value={isNumber(speed) ? speed : 0}
+          onChange={handleSliderSpeedChange}
         />
       </Box>
+      <ValueField
+        value={speed}
+        onChange={handleInputSpeedChange}
+        onBlur={handleBlur}
+        label="Speed"
+        name="speed"
+        variant="outlined"
+        unit="mm/s"
+        defaultValue={10}
+        inputProps={{min, max, step: 1}}
+      />
     </Box>
   );
 }
@@ -36,20 +71,24 @@ export function LinearBushingControl(props: LinearBushingControlProps) {
 // eslint-disable-next-line no-redeclare
 interface MyOutlinedTextFieldProps extends OutlinedTextFieldProps {
   unit: string;
+  inputProps?: InputBaseComponentProps;
 }
 
 const ValueField = React.memo((props: MyOutlinedTextFieldProps) => {
-  const {unit} = props;
+  const {unit, inputProps} = props;
   return (
     <TextField
       size="small"
       // margin="none"
       {...props}
       InputProps={{
-        endAdornment: <InputAdornment position="end">{unit}</InputAdornment>
+        endAdornment: <InputAdornment position="end">{unit}</InputAdornment>,
+        type: 'number',
+        'aria-labelledby': 'input-slider',
+        inputProps
       }}
       sx={{
-        marginRight: 3
+        marginLeft: 3
         // width: '15ch'
       }}
     />
