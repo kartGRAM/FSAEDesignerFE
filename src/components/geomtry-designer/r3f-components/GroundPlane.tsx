4269ba22a88bbb79f0e5bd33bd276d63406e4624
kartGRAM <ahuaya1024@gmail.com>
2022-12-04 15:30:12 +0900

    GridをHelperに変更

--- src/components/geomtry-designer/r3f-components/GroundPlane.tsx
+++ src/components/geomtry-designer/r3f-components/GroundPlane.tsx
@@ -2,42 +2,22 @@
 import * as React from 'react';
 import * as THREE from 'three';
 import {useFrame} from '@react-three/fiber';
-import {Plane} from '@react-three/drei';
 
-import {getMatrix3} from '@gd/NamedValues';
 import {useSelector} from 'react-redux';
 import {RootState} from '@store/store';
 
 const GroundPlane = () => {
-  const coMatrix = getMatrix3(
-    useSelector((state: RootState) => state.dgd.present.transCoordinateMatrix)
-  );
-
   const grid = useSelector(
     (state: RootState) => state.uigd.present.gdSceneState.showGroundPlaneGrid
   );
 
   useFrame(() => {});
 
-  const length = 5000;
+  const length = 8000;
   const segmentLength = 200;
   const segments = Math.floor(length / segmentLength);
 
   if (!grid) return null;
-  return (
-    <Plane
-      args={[length, length, segments, segments]}
-      position={[0, 0, 0]}
-      rotation={new THREE.Euler().setFromRotationMatrix(
-        new THREE.Matrix4().setFromMatrix3(coMatrix)
-      )}
-    >
-      <meshBasicMaterial
-        args={[{color: 0x999999}]}
-        wireframe
-        wireframeLinewidth={1}
-      />
-    </Plane>
-  );
+  return <gridHelper args={[length, segments, 0x999999, 0x999999]} />;
 };
 export default GroundPlane;
