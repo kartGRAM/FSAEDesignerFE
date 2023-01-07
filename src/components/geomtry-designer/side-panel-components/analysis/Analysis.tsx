46f48e8630c68e96f4e078886a99b218ab541f8b
kartGRAM <ahuaya1024@gmail.com>
2023-01-07 14:05:28 +0900

    Analysisを追加

--- src/components/geomtry-designer/side-panel-components/analysis/Analysis.tsx
+++ src/components/geomtry-designer/side-panel-components/analysis/Analysis.tsx
@@ -14,6 +14,8 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import FolderIcon from '@mui/icons-material/Folder';
 import DeleteIcon from '@mui/icons-material/Delete';
+import AvTimer from '@mui/icons-material/AvTimer';
+import TimeLine from '@mui/icons-material/Timeline';
 import AccountTreeIcon from '@mui/icons-material/AccountTree';
 import Toolbar from '@mui/material/Toolbar';
 import {alpha} from '@mui/material/styles';
@@ -27,79 +29,97 @@ export default function Controllers() {
       <Box
         component="div"
         display="flex"
+        flexDirection="column"
         alignItems="center"
         justifyContent="center"
-        padding={3}
       >
-        <Button variant="contained" size="large" onClick={() => {}}>
-          Create a New Test
-        </Button>
-      </Box>
-      <Box
-        component="div"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        padding={3}
-        sx={{backgroundColor: '#FFF'}}
-      >
-        <List>
-          {generate(
-            <ListItem>
-              <Button sx={{color: '#222'}}>
-                <ListItemAvatar>
-                  <Avatar>
-                    <AccountTreeIcon />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  primary="Single-line item"
-                  secondary="Secondary text"
-                />
-              </Button>
-              <Toolbar
-                sx={{
-                  paddingLeft: '0px!important',
-                  minHeight: '24px!important',
-                  justifyContent: 'center',
-                  zIndex: 1,
-                  background: alpha('#FFFFFF', 0.0)
-                }}
-              >
-                <IconButton
-                  edge="end"
-                  aria-label="delete"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    e.preventDefault();
+        <Box component="div" padding={3}>
+          <Button variant="contained" size="large" onClick={() => {}}>
+            Create a New Test
+          </Button>
+        </Box>
+        <Box
+          component="div"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          padding={3}
+          sx={{backgroundColor: '#FFF', minWidth: 'fit-content', width: '100%'}}
+        >
+          <List sx={{whiteSpace: 'nowrap', width: '100%'}}>
+            {generate(
+              <ListItem>
+                <Button sx={{color: '#222'}}>
+                  <ListItemAvatar>
+                    <Avatar>
+                      <AccountTreeIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Single-line item"
+                    secondary="Secondary text"
+                  />
+                </Button>
+                <Toolbar
+                  sx={{
+                    paddingLeft: '0px!important',
+                    paddingRight: '0px!important',
+                    minHeight: '24px!important',
+                    justifyContent: 'right',
+                    flexGrow: 1,
+                    zIndex: 1,
+                    background: alpha('#FFFFFF', 0.0)
                   }}
                 >
-                  <DeleteIcon />
-                </IconButton>
-                <IconButton
-                  edge="end"
-                  aria-label="delete"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    e.preventDefault();
-                  }}
-                >
-                  <DeleteIcon />
-                </IconButton>
-                <IconButton
-                  edge="end"
-                  aria-label="delete"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    e.preventDefault();
-                  }}
-                >
-                  <DeleteIcon />
-                </IconButton>
-              </Toolbar>
-            </ListItem>
-          )}
-        </List>
+                  <IconButton
+                    sx={{ml: 1}}
+                    edge="end"
+                    aria-label="folder"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      e.preventDefault();
+                    }}
+                  >
+                    <FolderIcon />
+                  </IconButton>
+                  <IconButton
+                    sx={{ml: 2}}
+                    edge="end"
+                    aria-label="reload"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      e.preventDefault();
+                    }}
+                  >
+                    <AvTimer />
+                  </IconButton>
+                  <IconButton
+                    sx={{ml: 2}}
+                    edge="end"
+                    aria-label="graph"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      e.preventDefault();
+                    }}
+                  >
+                    <TimeLine />
+                  </IconButton>
+                  <IconButton
+                    sx={{ml: 2}}
+                    edge="end"
+                    aria-label="delete"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      e.preventDefault();
+                    }}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </Toolbar>
+              </ListItem>
+            )}
+          </List>
+        </Box>
       </Box>
     </>
   );
