c17e1264629301dac28afd56669c563c27ff13ae
kartGRAM <ahuaya1024@gmail.com>
2023-06-27 23:22:11 +0900

    ParameterSweeperを実装中

--- src/geometryDesigner/analysis/ParameterSweeper.ts
+++ src/geometryDesigner/analysis/ParameterSweeper.ts
@@ -20,7 +20,7 @@ export interface IParameterSweeper {
   readonly endValue: number;
   readonly stepValue: number;
   readonly divisionValue: number;
-  divisionMode: boolean;
+  mode: 'division' | 'step';
   getData(): IDataParameterSweeper;
 }
 
@@ -32,6 +32,7 @@ export interface IDataParameterSweeper {
   endFormula: IDataFormula;
   stepFormula: IDataFormula;
   divisionFormula: IDataFormula;
+  mode: 'division' | 'step';
 }
 
 function isDataParameterSweeper(data: any): data is IDataParameterSweeper {
@@ -56,18 +57,42 @@ export class ParameterSweeper implements IParameterSweeper {
     return 'error';
   }
 
-  valueFormula: Formula;
+  startFormula: Formula;
 
-  get evaluatedValue() {
-    return this.valueFormula.evaluatedValue;
+  endFormula: Formula;
+
+  stepFormula: Formula;
+
+  divisionFormula: Formula;
+
+  mode: 'division' | 'step';
+
+  get startValue() {
+    return this.startFormula.evaluatedValue;
+  }
+
+  get endValue() {
+    return this.endFormula.evaluatedValue;
+  }
+
+  get stepValue() {
+    return this.stepFormula.evaluatedValue;
+  }
+
+  get divisionValue() {
+    return this.divisionFormula.evaluatedValue;
   }
 
   getData(): IDataParameterSweeper {
     return {
       isDataParameterSweeper: true,
       type: this.type,
-      valueFormula: this.valueFormula.getData(),
-      target: this.target
+      startFormula: this.startFormula.getData(),
+      endFormula: this.endFormula.getData(),
+      stepFormula: this.stepFormula.getData(),
+      divisionFormula: this.divisionFormula.getData(),
+      target: this.target,
+      mode: this.mode
     };
   }
 
@@ -85,20 +110,47 @@ export class ParameterSweeper implements IParameterSweeper {
 
   constructor(
     params:
-      | {type: SweeperType; target: IDataControl; valueFormula: string}
+      | {
+          type: SweeperType;
+          target: IDataControl;
+          startFormula: string;
+          endFormula: string;
+          mode: 'division' | 'step';
+          divisionFormula?: string;
+          stepFormula?: string;
+        }
       | IDataParameterSweeper
   ) {
     this.type = params.type;
+    this.mode = params.mode;
     if (isDataParameterSweeper(params)) {
       const data = params;
       this.target = data.target;
-      this.valueFormula = new Formula(data.valueFormula);
+      this.startFormula = new Formula(data.startFormula);
+      this.endFormula = new Formula(data.endFormula);
+      this.stepFormula = new Formula(data.stepFormula);
+      this.divisionFormula = new Formula(data.divisionFormula);
     } else {
       this.target = params.target.nodeID;
-      this.valueFormula = new Formula({
-        name: 'SweeperValue',
-        formula: params.valueFormula,
-        absPath: `setterFor${this.target}`
+      this.startFormula = new Formula({
+        name: 'startValue',
+        formula: params.startFormula,
+        absPath: `startValueSetterFor${this.target}`
+      });
+      this.endFormula = new Formula({
+        name: 'endValue',
+        formula: params.endFormula,
+        absPath: `endValueSetterFor${this.target}`
+      });
+      this.stepFormula = new Formula({
+        name: 'stepValue',
+        formula: params.stepFormula ?? '1',
+        absPath: `stepValueFor${this.target}`
+      });
+      this.divisionFormula = new Formula({
+        name: 'divisionValue',
+        formula: params.divisionFormula ?? '1',
+        absPath: `divisionValueFor${this.target}`
       });
     }
   }
